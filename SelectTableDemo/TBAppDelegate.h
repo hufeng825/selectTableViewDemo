4aa68ca99aed114b6ecb82840e0d9706920f9eb7
hufeng.hf <hufeng.hf@alibaba-inc.com>
2014-07-09 00:53:31 +0800

    Initial Commit

---
+++ SelectTableDemo/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  SelectTableDemo
+//
+//  Created by jason on 14-7-9.
+//  Copyright (c) 2014年 ___taobao___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
